ba5c70b7e4b624e85f03c2a6c8bbbe0906f0cb35
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2023-02-16 07:15:17 -0600

    add work mode control

--- SERVO_42C/servo_control.py
+++ SERVO_42C/servo_control.py
@@ -1,21 +1,13 @@
 #imports
 import pyserial
 import time
+global output
 
 serial1 = serial.Serial('/dev/ttyUSB0') #x stepper
 serial2 = serial.Serial('/dev/ttyUSB1') #y stepper
 
-#sends calabrate command
-def calabrate():
-    if stepper1 == 'X':
-        serial1.write('e0 80 00 60')
-    elif stepper1 == 'Y'
-        serial1.write('e1 80 00 60')
-    else:
-        return 10
-    time.sleep(60)
-    #reads success or fail output
-    output = serial1.read(8)
+#reads success or fail output
+def _return():
     result = output[3:5]
     if result == '01':
         return 0
@@ -23,7 +15,54 @@ def calabrate():
         return 1
     else:
         return 11
+
+#sends calabrate command
+def calabrate():
+    if stepper1 == 'X':
+        serial1.write('e0 80 00 60')
+        time.sleep(60)
+        output = serial1.read(8)
+    elif stepper1 == 'Y'
+        serial2.write('e1 80 00 60')
+        time.sleep(60)
+        output = serial2.read(8)
+    else:
+        return 10
+    _return()
+
+#sets work mode
+def work_mode(mode):
+    #sends open commands
+    if mode == 'open':
+        if stepper1 == 'X':
+            serial1.write('e0 82 00 63')
+            time.sleep(1)
+            output = serial1.read(8)
+        elif stepper1 == "Y":
+            serial2.write('e1 82 00 63')
+            time.sleep(1)
+            output = serial2.read(8)
+        else:
+            return 10
+    #sends close commands
+    elif mode == 'closed':
+        if stepper1 == 'X':
+            serial1.write('e0 82 01 63')
+            time.sleep(1)
+            output = serial1.read(8)
+        elif stepper1 == 'Y':
+            serial2.write('e1 82 01 63')
+            time.sleep(1)
+            output = serial2.read(8)
+        else:
+            return 10
+    else:
+        return 10
+    _return()
+            
 #main body
 if __name__ == '__main__':
     if command == 'cal':
         calabrate()
+    elif command == 'work':
+        work_mode()
